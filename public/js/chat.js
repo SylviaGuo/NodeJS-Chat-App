3cfe2e78c4e9359456405b8d158ce564e6527419
Sylvia <sylviaguo31@gmail.com>
2021-01-09 20:22:00 +1300

    sending messages to rooms, render user list, automatic scrolling

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,14 +6,41 @@ const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 const $shareLocationButton = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
+const $sidebar = document.querySelector('#sidebar')
 
 //Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMsgTemplate = document.querySelector('#location-message-template').innerHTML
-
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 //Options
 const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true})
+
+const autoscroll = () => {
+    //new message element
+    const $newMessage = $messages.lastElementChild
+
+    //height of the new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+    console.log('New Message Height:', newMessageHeight)
+    
+    //visible height
+    const visibleHeight = $messages.offsetHeight
+
+    //height of messages container
+    const containerHeight = $messages.scrollHeight
+
+    //how far have I scrolled?
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if(containerHeight - newMessageHeight <= scrollOffset){
+        $messages.scrollTop = $messages.scrollHeight - visibleHeight
+    }
+
+
+}
 
 socket.emit('join', {username, room}, (error) => {
     if(error){
@@ -24,19 +51,31 @@ socket.emit('join', {username, room}, (error) => {
 
 socket.on('message', (msg) => {
     const html = Mustache.render(messageTemplate, {
+        username: msg.username,
         msg: msg.text,
         createdAt: moment(msg.createdAt).format('MM-DD HH:mm:ss')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
+})
+
+socket.on('roomInfo', ({room, users}) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    $sidebar.innerHTML = html
 })
 
 socket.on('locationMessage', (msg) => {
     console.log(msg)
     const html = Mustache.render(locationMsgTemplate, {
+        username: msg.username,
         url: msg.url,
         createdAt: moment(msg.createdAt).format('MM-DD HH:mm:ss')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
 })
 
 $messageForm.addEventListener('submit', (e) => {
